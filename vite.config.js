c2ad91452b51b698f82d9cde0fd5ee25caff8d6c
Ethan Deeley <103002207+NinZargo@users.noreply.github.com>
2025-08-12 14:38:02 +0100

    Fix to deploy to brunelsailingsignup.co.uk via github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SignupForm/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
